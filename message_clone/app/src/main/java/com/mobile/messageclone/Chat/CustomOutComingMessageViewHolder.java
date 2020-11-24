ce00f6ba0207e4b375dbe84ce6004889b98dfd89
Nguyen Thi Ngoc Tram <ntntram.013@gmail.com>
2020-11-24 11:31:37 +0700

    Revert "Revert "Revert "Checkin internet connection need a bit more time"""
    This reverts commit f01e0089ffb345ab8c36e39c37fb452e0b6b699f.

--- message_clone/app/src/main/java/com/mobile/messageclone/Chat/CustomOutComingMessageViewHolder.java
+++ message_clone/app/src/main/java/com/mobile/messageclone/Chat/CustomOutComingMessageViewHolder.java
@@ -3,7 +3,6 @@ package com.mobile.messageclone.Chat;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 
 import com.mobile.messageclone.R;
 import com.stfalcon.chatkit.messages.MessageHolders;
@@ -15,28 +14,11 @@ public class CustomOutComingMessageViewHolder extends MessageHolders.OutcomingTe
 
     @Override
     public void onBind(LibMessage message) {
-
+        Log.d("TAG","HUHU");
         super.onBind(message);
 
         ImageView tickIcon=itemView.findViewById(R.id.SeenStatus);
-        ProgressBar progressBar=itemView.findViewById(R.id.ProgressBarSending);
-        if (message.getIsSeen()==true)
-        {
-            progressBar.setVisibility(View.GONE);
-            Log.d("TAG","HUHU");
-        }
-        else
-        {
-            progressBar.setVisibility(View.VISIBLE);
-        }
-        //if (message.getIsSeen()==true)
-        //{
-       //     tickIcon.setImageResource(R.drawable.ic_double_tick_indicator);
-       // }
-       // else if (message.getIsSeen()==false)
-       // {
-        //    tickIcon.setImageResource(R.drawable.ic_baseline_check_24);
-       // }
+
 
     }
 }
